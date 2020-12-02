31b880d026fa16f05a36573f537433cd16654625
sagacious-shell-7968 <tommysytc.tc@gmail.com>
2020-12-02 08:37:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let animal = ""
 
-function myAnimal() {
+function myAnimal(animal) {
   return (animal);
 }
 
-function yourAnimal() {
+function yourAnimal(cat) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return cat;
 }
 
 function add2(n) {
